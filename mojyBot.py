555686c6691fb23939396ebc14a3212f67d0121d
mohammad reza azizi <78251072+mojy150@users.noreply.github.com>
2024-06-21 12:25:59 +0330

    create function about /start

--- mojyBot.py
+++ mojyBot.py
@@ -1,10 +1,26 @@
 import telebot
 # import PIL
 import os
+from telebot import types
+
 f = open("my bot token.txt","r+")
 TOKEN = f.readline()
 bot = telebot.TeleBot(TOKEN)
 
+def startcmd(user):
+    usertext = user.text
+    userchatid = user.chat.id
+    userusername = user.chat.username
+    userfirstname = user.chat.first_name
+    
+    dokmeha = types.ReplyKeyboardMarkup(row_width=3)
+    dokme1 = types.KeyboardButton("test1")
+    dokme2 = types.KeyboardButton("test2")
+    dokme3 = types.KeyboardButton("test3")
+    dokmeha.add(dokme1,dokme2,dokme3)
+    
+    bot.send_message(userchatid,"Hello, wellcome to mojy bot!",reply_markup=dokmeha)
+# --------------------------------------
 @bot.message_handler(content_types=["text"])
 def botmain(user):
     usertext = user.text
@@ -12,11 +28,10 @@ def botmain(user):
     userusername = user.chat.username
     userfirstname = user.chat.first_name
     
+    if usertext == "/start":
+        startcmd(user)
     
     
-    
-    # --------------------------------------
-    bot.send_message(userchatid,"salam chetori?")
     # --------------------------------------
     print("new message from:",userchatid)
     print("username:",userusername)
